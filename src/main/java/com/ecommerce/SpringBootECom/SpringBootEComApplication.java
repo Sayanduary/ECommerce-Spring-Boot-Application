e58762b6965db221b6fcb39c803c33b15dc5c15b
Sayan Duary <sayanduary@gmail.com>
2025-08-19 19:23:06 +0530

    Spring Initializer

---
+++ src/main/java/com/ecommerce/SpringBootECom/SpringBootEComApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.SpringBootECom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEComApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEComApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/SpringBootECom/SpringBootEComApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.SpringBootECom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEComApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
