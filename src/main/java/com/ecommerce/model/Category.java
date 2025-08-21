b392923a551459a4798b53cb35ef99b12ac60e30
Sayan Duary <sayanduary@gmail.com>
2025-08-21 20:56:48 +0530

    Spring Initializer

--- src/main/java/com/ecommerce/model/Category.java
+++ src/main/java/com/ecommerce/model/Category.java
@@ -1,13 +1,16 @@
 package com.ecommerce.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity(name = "categories")
 public class Category {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
-    private String categoryName;
+    private String categoryName;        
 
     // ✅ No-args constructor (needed for JSON deserialization)
     public Category() {}
